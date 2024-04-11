267810089609c8300cc60c63e1d63f94d04e1280
Sergei Legkodymov <rutven@gmail.com>
2024-04-11 21:00:34 +0300

    added rest and db services stubs

---
+++ rest-service/src/main/java/name/legkodymov/poc/kafka/restservice/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package name.legkodymov.poc.kafka.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
